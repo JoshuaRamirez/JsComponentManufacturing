d6883aa5c9e302de9ae844da273a19a1751bccd9
Joshua Ramirez <joshuaramirezdeveloper@gmail.com>
2012-07-05 20:18:20 -0500

    Fixed a massive bug that I accidentally put in :)

--- ComponentManufacturing.js
+++ ComponentManufacturing.js
@@ -80,7 +80,6 @@ $(document).ready(
                 finish:function () {
                     var product = this.product;
                     product = packaging.makeInterface.call(product);
-                    this.product = null;
                     return product;
                 }
             };
